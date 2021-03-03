4439e695a04b7e677db134b8b43ebb7e28bb1d86
jaunn-victorr <79698388+jaunn-victorr@users.noreply.github.com>
2021-03-03 14:47:52 -0300

    index.php/02

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 echo 'Olá Mundo !!! \n';
+$saldo = 1000,00;
+$saque = 1000,00;
+
+if(saldo <= saque) {
+  $saldo - $saque;
+  echo 'sacado R$' . $saque ."\n"
+} else {
+  echo "valor do saque maior que o saque \n"
+}
